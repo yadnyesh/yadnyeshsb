cf98e85c9a777838e2b2a60026d865e23a82b017
Yadnyesh <yadnyesh.juvekar@gmail.com>
2021-08-16 18:13:22 +0530

    Hello from Spring

---
+++ src/main/java/io/yadnyesh/fullper/controller/TestController.java
@@ -0,0 +1,14 @@
+package io.yadnyesh.fullper.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String sendhello() {
+        return "Hello!";
+    }
+
+}
