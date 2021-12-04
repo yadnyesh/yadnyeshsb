74dd6c4dcd7516b140a611437e42595934084129
yadnyesh <yadnyesh.juvekar@gmail.com>
2021-12-04 19:10:41 +0530

    Fetch list of APIs in Synchronous way

--- src/main/java/io/yadnyesh/fullper/service/CountryClient.java
+++ src/main/java/io/yadnyesh/fullper/service/CountryClient.java
@@ -7,6 +7,7 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.CompletableFuture;
 
 @Service
@@ -18,6 +19,13 @@ public class CountryClient {
         String url = "https://restcountries.com/v2/lang/" + language + "?fields=name";
         Country[] response = restTemplate.getForObject(url, Country[].class);
         log.info("LANGUAGE: {} Fetched all relevant countries.",language);
+        try {
+            log.info("LANGUAGE Thread going into hibernation....");
+            Thread.sleep(4000);
+            log.info("LANGUAGE Thread out of hibernation....");
+        } catch (InterruptedException e) {
+            log.error(e.getMessage());
+        }
         return Arrays.asList(response);
     }
 
@@ -25,6 +33,13 @@ public class CountryClient {
         String url = "https://restcountries.com/v2/regionalbloc/" + region + "?fields=name";
         Country[] response = restTemplate.getForObject(url, Country[].class);
         log.info("REGION: {} Fetched all relevant countries.",region);
+        try {
+            log.info("REGION Thread going into hibernation....");
+            Thread.sleep(3000);
+            log.info("REGION Thread out of hibernation....");
+        } catch (InterruptedException e) {
+            log.error(e.getMessage());
+        }
         return Arrays.asList(response);
     }
 
